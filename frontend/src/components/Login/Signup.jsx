6f8dbead3def773ea27e2bd47858e6b284a1bc37
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-10-12 22:22:26 +0500

    added

--- frontend/src/components/Login/Signup.jsx
+++ frontend/src/components/Login/Signup.jsx
@@ -23,7 +23,7 @@ const Signup = () => {
                     <form className='space-y-6'>
 
                         <div>
-                            <label className='block text-sm font-medium text-gray-700' htmlFor="email">Email Address</label>
+                            <label className='block text-sm font-medium text-gray-700' htmlFor="email">Email Address</label> 
                             <div className='mt-1'>
                                 <input
                                     className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
